04a7b1ae51888f2aee5fbd2bb3570bedcafaf106
Luke Early <23438839+LukeEarly@users.noreply.github.com>
2018-04-18 15:38:49 -0400

    change State to class and add stop method

--- src/State.java
+++ src/State.java
@@ -1,3 +1,18 @@
-public interface State {
-    void run() throws FSMException;
+public class State {
+    private Runnable runnable;
+    private Runnale stopable;
+    public State(Runnable runnable, Runnable stoppable) {
+        this.runnable = runnable;
+        this.stopable = stoppable;
+    }
+    public State(Runnable runnable) {
+        Runnable stoppable = () -> ;
+        this(runnable, stoppable;
+    }
+    void run() throws FSMException {
+        runnable.run();
+    }
+    void stop(){
+        stoppable.run();
+    }
 }
